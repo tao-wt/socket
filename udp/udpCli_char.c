c5195e3a16331ec7b8a760036a92d78289fa6992
taowang <xx201890@126.com>
2018-06-13 22:27:00 +0800

    :test stdin buff

---
+++ udp/udpCli_char.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include <string.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define	max(a,b)    ((a) > (b) ? (a) : (b))
+#define SA struct sockaddr
+#define LISTENQ 1024 /* 2nd argument to listen() */
+#define SERV_PORT 9876 /* TCP and UDP */
+#define MAXLINE 4096 /* max text line */
+
+
+void dg_cli(FILE *fp, int sockfd, const SA * pservaddr, socklen_t servlen)
+{
+    int n;
+    char c, rc, sendline[MAXLINE], recvline[MAXLINE + 1];
+
+    while((c = fgetc(fp)) != -1){
+        sendto(sockfd, &c, 1, 0, pservaddr, servlen);
+
+        n = recvfrom(sockfd, &rc, 1, 0, NULL, NULL);
+
+        fputc(rc, stdout);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+    uint16_t serPort;
+
+    if (argc != 3){
+        printf("usage:udpcli ip port\n");
+        exit(1);
+    }
+
+    sscanf(argv[2], "%d", &serPort);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+    servaddr.sin_port = htons(serPort);
+
+    dg_cli(stdin, sockfd, (SA *) &servaddr, sizeof(servaddr));
+    exit(0);
+}
